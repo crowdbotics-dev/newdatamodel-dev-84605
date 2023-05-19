01e43cc3ea1133c7642ab59a8a26762f958af05d
Crowdbotics <engineering@crowdbotics.com>
2023-05-19 05:34:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/newdatamodel_dev_84605/MainActivity.java
@@ -0,0 +1,15 @@
+package com.newdatamodel_dev_84605;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "newdatamodel_dev_84605";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "newdatamodel-dev-84605",
+  url: "https://newdatamodel-dev-84605.botics.co",
+  api: "https://newdatamodel-dev-84605.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
